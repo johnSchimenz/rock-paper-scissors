76bc3a198b9c94d5623fffc60b8efa0bd6cdcc01
John Schimenz <john.schimenz@gmail.com>
2023-07-17 08:45:50 -0500

    Make functions getComputerChoice and playRound

--- main.js
+++ main.js
@@ -3,23 +3,76 @@
 // Output is either "win", "lose", or "draw"
 // Win, lose, and draw conditions based on rock, paper, scissors rules
 
-// Computer randomly choses rock, paper, or scissors
+// Player chooses rock, paper or scissors
+
+
+function getPlayerChoice()
+{
+    let playerChoice = prompt("Do you have what it takes to win against the computer?!?!  Type in ROCK, PAPER, or SCISSORS!");
+    playerChoice = playerChoice.trim();
+    playerChoice = playerChoice.toLowerCase();
+    let uppercaseFirstLetter = playerChoice.slice(0, 1);
+    uppercaseFirstLetter = uppercaseFirstLetter.toUpperCase();
+    playerChoice = playerChoice.replace(playerChoice.charAt(0), uppercaseFirstLetter);
+    console.log(`Player chooses ${playerChoice}`);
+    return playerChoice;
+}
+
+// Computer randomly chooses Rock, Paper, or Scissors
 function getComputerChoice()
 {
     const min = 0;
     const max = 3;
-    return Math.floor(Math.random() * (max - min) + min);
+    let computerRandomNumber = Math.floor(Math.random() * (max - min) + min);
+
+    if (computerRandomNumber === 0)
+    {
+        console.log(`Computer chooses Rock`);
+        return "Rock";
+    }
+    else if (computerRandomNumber === 1)
+    {
+        console.log(`Computer chooses Paper`);
+        return "Paper";
+    }
+    else if (computerRandomNumber === 2)
+    {
+        console.log(`Computer chooses Scissors`);
+        return "Scissors";
+    }
+    else{
+        return "ERROR!!! Check on getComputerChoice function";
+    }
 }
 
 // User inputs rock, paper, or scissors
 function playRound(playerSelection, computerSelection)
 {
-    
+    // Result 1: You win
+    if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper"))
+    {
+        return `You win!  ${playerSelection} beats ${computerSelection}`;
+    }
+
+    // Result 2: You lose
+    else if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock"))
+    {
+        return `You lose!  ${computerSelection} beats ${playerSelection}`;
+
+    }
+
+    // Result 3: You tie
+    else if (playerSelection === computerSelection)
+    {
+        return `You tie!`;
+    }
+    // Result 4: Invalid entry
+    else 
+    {
+        return `Try again.  ${playerSelection} is not a valid choice.  You must choose either rock, paper, or scissors.`
+    }
 }
 
+function game() {};
 
-    // If none of the 3 chosen, prompt to pick again
-// Results
-    // If win, state "winner"
-    // If lose, state "loser"
-    // If tie, state "draw"
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
